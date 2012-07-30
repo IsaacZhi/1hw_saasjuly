79a08682bebb5b6e82cd9368b3181054a3fb7908
Isaac Zhi <QFWFQzhi@gmail.com>
2012-07-30 02:27:32 +0000

    part 6 local ok

---
+++ hw1p6.rb
@@ -0,0 +1,25 @@
+class CartesianProduct
+  include Enumerable
+  def initialize(first, second)
+    @firstarray = first
+    @secondarray = second
+  end
+
+  def each
+    result = []
+    return result if nil == @firstarray || nil == @secondarray
+    return result if @firstarray.length == 0 || @secondarray.length == 0
+    @firstarray.each do |item1|
+      @secondarray.each do |item2|
+        yield [item1, item2]
+      end
+    end
+  end
+end
+
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+
